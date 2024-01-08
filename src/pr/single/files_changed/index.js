9c7b13f288153b2c45a1f42775c8f09186c641da
Marcos Santos <marcosv.santos@gruposbf.com.br>
2024-01-08 13:46:45 -0300

    feat(files-changed): new function
    * filter data changes based on path or filename

--- src/pr/single/files_changed/index.js
+++ src/pr/single/files_changed/index.js
@@ -1,3 +1,18 @@
-function getChangedFilesFrom(folder: string, tree: HTMLCollection) {
-  return (  );
+function getChangedFilesFromFolder(folderPath='') {
+
+  if (!folderPath) throw Error(`getChangedFilesFrom expect folder argument`);
+
+  var spansWithFilterableText = document.querySelectorAll('span[data-filterable-item-text]');
+
+  var filteredFiles = [];
+
+  spansWithFilterableText.forEach(span => {
+    var textContent = span.textContent.trim();
+    if (textContent.includes(folderPath)) {
+      filteredFiles.push(textContent);
+    }
+  });
+
+
+  return filteredFiles;
 };
